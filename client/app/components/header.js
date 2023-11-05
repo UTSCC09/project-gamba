b6945930d9cf08e2e40a4b0922737d174c2efe2f
Harris Chong <harrisbchong@gmail.com>
2023-11-05 16:26:06 -0500

    signout logic and display

--- client/app/components/header.js
+++ client/app/components/header.js
@@ -1,19 +1,33 @@
 import Link from 'next/link'
 import Cookies from "js-cookie";
-import {useRouter} from 'next/navigation'
+import { useRouter } from 'next/navigation'
+import { useState, useEffect } from 'react';
 
-export default function Header(){
+export default function Header() {
     const router = useRouter()
+    const [isUserLoggedIn, setIsUserLoggedIn] = useState(Cookies.get('username'));
+
+    // Listen for changes in the 'username' cookie
+    useEffect(() => {
+        const username = Cookies.get('username');
+        setIsUserLoggedIn(username);
+    }, []);
+
     const handleSignOut = async (event) => {
         event.preventDefault();
         Cookies.remove('username');
+        setIsUserLoggedIn(null); // Update the state to trigger a re-render
         router.push('/');
     }
-    return( 
+
+    return (
         <nav>
             <div>
                 <Link href="/signup">Signup</Link>
-                <button onClick={handleSignOut}>Signout</button>
+                {isUserLoggedIn ? (
+                    <button onClick={handleSignOut}>Signout</button>
+                ) : null}
+
             </div>
         </nav>
     )
