57c21a6d64189e1221df9cfc75c8acaee544f69e
markl90001 <markl90001@gmail.com>
2023-11-12 16:49:46 -0500

    Made spin use actual items with images

--- client/app/home/page.js
+++ client/app/home/page.js
@@ -13,14 +13,6 @@ export default function Home() {
         setIsUserLoggedIn(username);
     }, []);
 
-    let blue = ["b1", "b2", "b3", "b4", "b5"];
-    let purple = ["p1", "p2", "p3", "p4"];
-    let pink = ["pi1", "pi2", "pi3"];
-    let red = ["r1", "r2"];
-    let knife = ["k1", "k2", "k3", "k4", "k5"];
-
-    console.log(blue);
-
     return (
         <>
             <div>
@@ -28,7 +20,7 @@ export default function Home() {
                     <div>
                         <Header />
                         <div>Home</div>
-                        <CaseSpin blue={blue} purple={purple} pink={pink} red={red} knife={knife} />
+                        <CaseSpin caseName={"chroma"} />
                     </div>
                 ) : <div>You are not authenticated. Please log in.</div>}
             </div>
