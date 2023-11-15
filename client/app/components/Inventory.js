2684b98e02692e1a38d34b217547bebb457cb0e3
Harris Chong <harrisbchong@gmail.com>
2023-11-15 16:20:21 -0500

    added inventory filter

--- client/app/components/Inventory.js
+++ client/app/components/Inventory.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react';
 import { GET_ITEMS } from '../queries/userQueries'
 import { useQuery } from '@apollo/client'
 import Cookies from 'js-cookie';
@@ -7,6 +8,14 @@ import './Inventory.css'
 export default function Inventory() {
     const username = Cookies.get('username');
     const { loading, error, data } = useQuery(GET_ITEMS, { variables: { username: username } });
+
+    const [filters, setFilters] = useState({
+        sortDirection: '',
+        caseName: '',
+        rarity: '',
+        quality: '',
+    });
+
     let inventory = [];
 
     if (!loading && !error) {
@@ -17,14 +26,109 @@ export default function Inventory() {
     if (loading) return <p>Loading...</p>
     if (error) return <p>Something Went Wrong</p>
 
+    const handleFilterChange = (filterType, value) => {
+        setFilters((prevFilters) => ({ ...prevFilters, [filterType]: value }));
+    };
+
+    const applyFilters = (item) => {
+        const { sortDirection, caseName, rarity, quality } = filters;
+
+        return (
+            (!sortDirection || sortDirection === 'asc' || sortDirection === 'desc') &&
+            (!caseName || item.case === caseName) &&
+            (!rarity || item.rarity === rarity) &&
+            (!quality || item.quality === quality)
+        );
+    };
+
+    const filteredInventory = inventory
+        .filter(applyFilters)
+        .sort((a, b) => {
+            if (filters.sortDirection === 'asc') {
+                return a.price - b.price;
+            } else if (filters.sortDirection === 'desc') {
+                return b.price - a.price;
+            } else {
+                return 0;
+            }
+        });
+
     return (
         <>
-            <div>filter</div>
+            <div>
+                <label>
+                    Price:
+                    <select
+                        value={filters.sortDirection}
+                        onChange={(e) =>
+                            handleFilterChange('sortDirection', e.target.value)
+                        }
+                    >
+                        <option value="">Default</option>
+                        <option value="asc">Lowest to Highest</option>
+                        <option value="desc">Highest to Lowest</option>
+                    </select>
+                </label>
+                <label>
+                    Case Name:
+                    {/* Dropdown for case name */}
+                    <select
+                        value={filters.caseName}
+                        onChange={(e) => handleFilterChange('caseName', e.target.value)}
+                    >
+                        {/* Add options based on available case names */}
+                        <option value="">All</option>
+                        <option value="chroma">chroma</option>
+                        <option value="chroma2">chroma2</option>
+                        <option value="chroma3">chroma3</option>
+                        <option value="falchion">falchion</option>
+                        <option value="gamma">gamma</option>
+                        <option value="gamma2">gamma2</option>
+                        {/* Add more options as needed */}
+                    </select>
+                </label>
+                <label>
+                    Rarity:
+                    {/* Dropdown for rarity */}
+                    <select
+                        value={filters.rarity}
+                        onChange={(e) => handleFilterChange('rarity', e.target.value)}
+                    >
+                        {/* Add options based on available rarities */}
+                        <option value="">All</option>
+                        <option value="blue">blue</option>
+                        <option value="purple">purple</option>
+                        <option value="pink">pink</option>
+                        <option value="red">red</option>
+                        <option value="yellow">knives</option>
+                        {/* Add more options as needed */}
+                    </select>
+                </label>
+                <label>
+                    Quality:
+                    {/* Dropdown for quality */}
+                    <select
+                        value={filters.quality}
+                        onChange={(e) => handleFilterChange('quality', e.target.value)}
+                    >
+                        {/* Add options based on available qualities */}
+                        <option value="">All</option>
+                        <option value="Battle-Scarred">Battle-Scarred</option>
+                        <option value="Well-Worn">Well-Worn</option>
+                        <option value="Field-Tested">Field-Tested</option>
+                        <option value="Minimal Wear">Minimal Wear</option>
+                        <option value="Factory New">Factory New</option>
+                        {/* Add more options as needed */}
+                    </select>
+                </label>
+            </div>
             <div className="inventory-grid">
-                {inventory.map((item, index) => (
+                {filteredInventory.map((item, index) => (
                     <div key={index} className="inventory-item">
                         <img src={item.image} alt={item.skinName} />
-                        <p className="combined-names">{item.weaponName} | {item.skinName}</p>
+                        <p className="combined-names">
+                            {item.weaponName} | {item.skinName}
+                        </p>
                         <p>{item.quality}</p>
                         <p>Price: {item.price}</p>
                         <p>Quantity: {item.quantity}</p>
@@ -32,5 +136,5 @@ export default function Inventory() {
                 ))}
             </div>
         </>
-    )
+    );
 }
